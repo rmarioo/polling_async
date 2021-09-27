0c90481979f1ef9b1874292a9c1d1da1783b3aa9
mrusso <mrusso>
2021-09-28 00:05:36 +0200

    client with coroutines it saves threads

--- src/main/kotlin/spikes/asyncpizza/client/StatelessClient.kt
+++ src/main/kotlin/spikes/asyncpizza/client/StatelessClient.kt
@@ -1,8 +1,9 @@
 package spikes.asyncpizza.client
 
-import kotlinx.coroutines.CoroutineStart.LAZY
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
@@ -10,42 +11,54 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import spikes.asyncpizza.api.ResultWithStatus
+import java.util.concurrent.Executors
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
-class StatelessClient {
+class StatelessClient(val dispatcher: ExecutorCoroutineDispatcher) {
 
+    suspend fun doPollingSearch(good: String) = withContext(dispatcher) {
+        return@withContext search(good)
+    }
 
-     suspend fun search(good: String): ResponseEntity<ResultWithStatus> {
+    suspend fun search(good: String): ResponseEntity<ResultWithStatus> {
 
         var responseEntity = remoteSearch(good)
 
-        val resultWithStatus = responseEntity.body!!
+        val resultWithStatus: ResultWithStatus = responseEntity.body!!
 
-        if (resultWithStatus.responseStatus.status == "completed")
-            return responseEntity
-        else
-        {
-           log.info("received ${resultWithStatus.responseStatus}")
-           val retryAfterSeconds = resultWithStatus.responseStatus.retryAfterSeconds
-           log.info("i will retry in ${retryAfterSeconds.toLong()} seconds")
-           delay(retryAfterSeconds.toLong()  * 1000 + 500)
-           responseEntity = remoteSearch(good)
+        return if (isStatusCompleted(resultWithStatus))
+            responseEntity
+        else {
+            printDebugInfo(resultWithStatus)
+            delay(retryInMilliseconds(resultWithStatus))
+            remoteSearch(good)
         }
 
-        return responseEntity
+    }
 
+    private fun isStatusCompleted(resultWithStatus: ResultWithStatus) =
+        statusFrom(resultWithStatus) == "completed"
+
+    private fun statusFrom(resultWithStatus: ResultWithStatus) =
+        resultWithStatus.responseStatus.status
+
+    private fun printDebugInfo(resultWithStatus: ResultWithStatus) {
+        log.info("received ${resultWithStatus.responseStatus}")
+        log.info("i will retry in ${retryInMilliseconds(resultWithStatus)} milliseconds")
+    }
+
+    private fun retryInMilliseconds(resultWithStatus: ResultWithStatus): Long {
+        val retryAfterSeconds = resultWithStatus.responseStatus.retryAfterSeconds
+        val retryInMilliseconds = retryAfterSeconds.toLong() * 1000 + 1000
+        return retryInMilliseconds
     }
 
     private fun remoteSearch(good: String): ResponseEntity<ResultWithStatus> {
 
         val url = "http://localhost:8080/api/smart/bake/${good}"
-       log.info("calling service $url")
+        log.info("calling service $url")
         return RestTemplate().getForEntity(url, ResultWithStatus::class.java)
-    }
-
-    suspend fun doSearch(good: String) = withContext(Dispatchers.IO) {
-       return@withContext search(good)
     }
 
     companion object {
@@ -57,23 +70,24 @@ class StatelessClient {
 @OptIn(ExperimentalTime::class)
 fun main() {
 
-
-    val client = StatelessClient()
+    val singleThreadDispatcher = Executors.newFixedThreadPool(1).asCoroutineDispatcher()
+    val client = StatelessClient(singleThreadDispatcher)
 
     runBlocking {
-        val deferredResult = Pair(
-            async(start = LAZY) { client.search("pizza") },
-            async(start = LAZY) { client.search("cookie") })
+
+        val deferredResponses = listOf(
+            async { client.doPollingSearch("pizza") },
+            async { client.doPollingSearch("cookie") })
+
 
         val (results, duration) = measureTimedValue {
-            val r1 = deferredResult.first.await()
-            val r2 = deferredResult.second.await()
-            val result1 = r1.body!!.result
-            val result2 = r2.body!!.result
+            val responses = deferredResponses.awaitAll()
+            val result1 = responses[0].body!!.result
+            val result2 = responses[1].body!!.result
             Pair(result1, result2)
         }
 
-        println("in ${duration.inWholeSeconds} seconds received ${results.first} ${results.second}")
+        println("in ${duration.inWholeSeconds} seconds received ${results.first} and ${results.second}")
     }
 
 }
